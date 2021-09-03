19b703166ee3c821683064b007f6825394b79142
fumiya <altema1217bbfumi@live.jp>
2021-09-03 18:36:51 +0900

    add: useCounter

---
+++ src/useCounter.js
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+
+export const useCounter = (initialState = 0) => {
+  const [count, setCount] = useState(initialState)
+  const decrement = () => setCount(count - 1)
+  const increment = () => setCount(count + 1)
+  return { count, decrement, increment }
+}
