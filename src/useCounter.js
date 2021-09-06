b8cf2b3d94e9fcfd8a307b0d2eb794547f7b4359
fumiya <altema1217bbfumi@live.jp>
2021-09-06 16:42:44 +0900

    update: useState

--- src/useCounter.js
+++ src/useCounter.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 export const useCounter = (initialState = 0) => {
   const [count, setCount] = useState(initialState)
-  const decrement = () => setCount(count - 1)
-  const increment = () => setCount(count + 1)
+  const decrement = () => setCount((prevCount) => prevCount - 1)
+  const increment = () => setCount((prevCount) => prevCount + 1)
   return { count, decrement, increment }
 }
