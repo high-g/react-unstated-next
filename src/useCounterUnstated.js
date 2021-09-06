0906c9184b0485c0652dab014f79855fa0a24d68
fumiya <altema1217bbfumi@live.jp>
2021-09-06 16:52:44 +0900

    update

--- src/useCounterUnstated.js
+++ src/useCounterUnstated.js
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import { createContainer } from 'unstated-next'
 
 const useCounter = (initialState = 0) => {
   const [count, setCount] = useState(initialState)
-  const decrement = () => setCount(count - 1)
-  const increment = () => setCount(count + 1)
+  const decrement = useCallback(() => setCount((prevCount) => prevCount - 1), [])
+  const increment = useCallback(() => setCount((prevCount) => prevCount + 1), [])
   return { count, decrement, increment }
 }
 
